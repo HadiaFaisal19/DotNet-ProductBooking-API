2299dfd884458ec5c45ae567832fbab05bcbfcec
Hadia Faisal <hadiafaisal1003@gmail.com>
2024-08-08 19:09:54 +0500

    added folder

---
+++ ClassLibraryModel/Cutomer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibraryModel
+{
+    public class Customer
+    {
+        [Key]
+        public string CustomerCnic { get; set; }
+        public required string CustomerName { get; set; }
+        public string CustomerContact { get; set; }
+
+        public string ReferenceName { get; set; }
+        public string ReferenceContact { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public DateOnly Dob { get; set; }
+    }
+}
+
